6af666c7eef06236a8f8cb90f452427874924e91
Abhi <50555642+14041999ai@users.noreply.github.com>
2020-12-31 20:32:35 -0800

    my website
    neelam tent

--- neelamtenthouse/views.py
+++ neelamtenthouse/views.py
@@ -7,7 +7,7 @@ from .models import Event
 from math import ceil
 
 
-def index(request):
+def gallery(request):
     all_img = []
     bhatt = Event.objects.values('title','id')
     img_tit = { item['title']  for item in bhatt}
@@ -17,10 +17,20 @@ def index(request):
      nSlides = n // 4 + ceil((n / 4) - (n // 4))
      all_img.append([imge])
      params = {'all_img': all_img}
-     print(params)
-    return render(request, 'neelamtenthouse/index.html',params)
-def gallery(request):
-    return HttpResponse("Hope you like our work!")
+
+    return render(request, 'neelamtenthouse/gallery.html',params)
+def index(request):
+    all_img = []
+    bhatt = Event.objects.values('title', 'id')
+    img_tit = {item['title'] for item in bhatt}
+    for title in img_tit:
+        imge = Event.objects.filter(title=title)
+        n = len(imge)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        all_img.append([imge])
+        params = {'all_img': all_img}
+        print(params)
+    return render(request,'neelamtenthouse/index.html',params)
 def contact(request):
     return HttpResponse("We are here!.")
 def faq(request):
